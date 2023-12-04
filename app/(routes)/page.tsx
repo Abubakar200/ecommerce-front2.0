87eb992bf85596e77c2c820c82dde0c3f60717fc
Abubakar200 <sheikhabubakr4@gmail.com>
2023-12-04 23:45:34 +0500

    .

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,12 @@
+import Container from "@/components/ui/container";
+
 const HomePage = () => {
     return ( 
-        <div>
-        Hello ecommerce store
+        <Container>
+        <div className="space-x-10 pb-10">
+
         </div>
+        </Container>
      );
 }
  
