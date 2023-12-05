f8674005a41dc4576ba3c4e6c95cc92e0f481251
Abubakar200 <sheikhabubakr4@gmail.com>
2023-12-06 00:09:06 +0500

    8.45 individual product screen

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,20 +3,21 @@ import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
-export const revalidate = 0 
+export const revalidate = 0;
 const HomePage = async () => {
-    const products = await getProducts({ isFeatured: true })
-    const billboard = await getBillboards("7684b0be-a688-4788-b75f-3b00f4cb96bf")
-    return ( 
-        <Container>
-        <div className="space-x-10 pb-10">
-            <Billboard data={billboard} />
-        </div>
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("7684b0be-a688-4788-b75f-3b00f4cb96bf");
+  return (
+    <Container>
+      <div className="space-x-10 pb-10">
+        <Billboard data={billboard} />
+
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products}/>
+          <ProductList title="Featured Products" items={products} />
         </div>
-        </Container>
-     );
-}
- 
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
